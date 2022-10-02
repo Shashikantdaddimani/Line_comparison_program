2c5f5c760d8f1285b6df87b6e7373ed731cb8a74
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-02 09:47:41 +0530

    Adding to calculating length of line program

--- src/com/line/comparison/Line_Comparison.java
+++ src/com/line/comparison/Line_Comparison.java
@@ -1,10 +1,30 @@
 package com.line.comparison;
-
+import java.util.*;
 public class Line_Comparison {
 
+	 void lengthCalculate() {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter x1 co-ordinate : ");
+	        int x1 = sc.nextInt();
+	        System.out.println("Enter x2 co-ordinate : ");
+	        int x2 = sc.nextInt();
+	        System.out.println("Enter y1 co-ordinate : ");
+	        int y1 = sc.nextInt();
+	        System.out.println("Enter y2 co-ordinate : ");
+	        int y2 = sc.nextInt();
+
+	        Double lenOfLineFirst = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+	        System.out.println("Length of first line is " + lenOfLineFirst);
+	    }
+
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to Line comparison computation program");
+		Line_Comparison lc = new Line_Comparison();
+		lc.lengthCalculate();
+	}
+		
 	}
 
-}
+
