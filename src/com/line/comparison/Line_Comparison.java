583ab65ec483287b4a5ef4521301d8bfafc852a2
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-02 09:29:54 +0530

    Added to display Message program

---
+++ src/com/line/comparison/Line_Comparison.java
@@ -0,0 +1,10 @@
+package com.line.comparison;
+
+public class Line_Comparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line comparison computation program");
+	}
+
+}
